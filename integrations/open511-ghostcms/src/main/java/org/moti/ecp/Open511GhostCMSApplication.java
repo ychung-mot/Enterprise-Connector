7a73af5d262d34a3b1b633332a11c26ad4fa0709
apuritch <aran.puritch@dxc.com>
2022-02-28 13:54:33 -0800

    Add zipking and logback with MDC

--- integrations/open511-ghostcms/src/main/java/org/moti/ecp/Open511GhostCMSApplication.java
+++ integrations/open511-ghostcms/src/main/java/org/moti/ecp/Open511GhostCMSApplication.java
@@ -1,9 +1,13 @@
 package org.moti.ecp;
 
+import org.apache.camel.zipkin.starter.CamelZipkin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.apache.camel.opentracing.starter.CamelOpenTracing;
 
 @SpringBootApplication
+//@CamelOpenTracing
+@CamelZipkin
 public class Open511GhostCMSApplication {
 
     /**
--- integrations/open511-ghostcms/src/main/java/org/moti/ecp/Open511GhostCMSRouter.java
+++ integrations/open511-ghostcms/src/main/java/org/moti/ecp/Open511GhostCMSRouter.java
@@ -15,8 +15,16 @@ public class Open511GhostCMSRouter extends RouteBuilder {
     @Override
     public void configure() {
         
+        //Open511 API GET 10 Events - only once
+        from("timer:open511events?fixedRate=true&period=60000")
+            .toD("http://${properties:open511.events}" +  "?httpMethod=GET" + "&${properties:open511.limit}")
+            .unmarshal(new JacksonDataFormat(Open511Events.class))
+            .marshal().jacksonxml(true)
+            // .to("stream:out")
+            // .log("Response code from the Open 511 call was: ${header.CamelHttpResponseCode}");
+
             // Generate token and set header for Ghost CMS authorization
-            from("timer:mytimer?repeatCount=1")
+          //  from("timer:mytimer?repeatCount=1")
             .setHeader("Authorization", method(GhostApiTokenGeneratorBean.class, "getToken"))
             //.log("${header.Authorization}")
             .log("Ghost CMS Token Generated")
@@ -24,18 +32,11 @@ public class Open511GhostCMSRouter extends RouteBuilder {
             // Get post details and capture date of last change as this will be required for publishing an update.
             .toD("http://${properties:ghostcms.ghostcmshostname}${properties:ghostcms.postid}" +  "?httpMethod=GET" )
             .log("Ghost CMS Page Call")
-  //          .to("log:out")
             .end(); 
 
 
 
-        //Open511 API GET 10 Events - only once
-        // from("timer:mytimer?repeatCount=1")
-        //     .toD("http://${properties:open511.events}" +  "?httpMethod=GET" + "&${properties:open511.limit}")
-        //     .unmarshal(new JacksonDataFormat(Open511Events.class))
-        //     .marshal().jacksonxml(true)
-        //     .to("stream:out")
-        //     .log("Response code from the Open 511 call was: ${header.CamelHttpResponseCode}");
+
     
          // Get Ghost CMS Posts 
         //  from("timer:mytimer?repeatCount=1")
